ce86c27e75e9df3c40c882e360fa21c23909d090
unknown <G00398256@gmit.ie>
2021-04-03 16:35:10 +0100

    quiz for week 6

---
+++ week06/week06-functions.py
@@ -0,0 +1,13 @@
+# week06-functions.py
+# This is a program from a lab sheet from week06 and below the code are answers to the questions 
+# Author: Anja Antolkovic
+
+def yo(a):
+    return a * 2
+
+# a. What will be printed out when this program is run?
+#    Answer: Nothing because we haven't ask the program to return the value.
+# b. What would you have to add to the program to get it to print out what 
+#    the calling the function yo(3) returns?
+#    Answer: Print function - print(yo(a)).
+
