4d11e4fd6719a4f379c4ebe525e41973efb2b6e2
unknown <G00398256@gmit.ie>
2021-04-03 16:34:26 +0100

    quiz for week 5

---
+++ week05/week05-datastructures.py
@@ -0,0 +1,43 @@
+# week05-datastructures.py
+# This is a program from a lab sheet from week05 and below the code are answers to the questions 
+# author: Anja Antolkovic
+
+
+numberOfQuestions = 5
+averageAge = 23.4
+debugMode = True
+name = "joe"
+ages = []
+months = ('Jan', 'Feb', 'Mar')
+book = {}
+stuff = [ 12 , 'Fred', False, {}]
+someone = dict(firstname = "joe")
+me = {
+     "firstName" : "Andrew", 
+     "teaching" : [{ 
+         "courseName" : "programming", 
+         "semester" : 1 
+    },{
+             "courseName" : "Data Representation", 
+             "semester" : 2 
+    }
+    ]
+}
+
+# a. numberOfQuestions - integer
+# b. averageAge- float
+# c. debugMode - boolean
+# d. name - string
+# e. ages - list
+# f. months - tuple
+# g. months[1] - string
+# h. book - dictionary
+# i. stuff - list
+# j. stuff[2] - boolean
+# k. someone - dictionary
+# l. someone["firstname"] - string
+# m. me - dictionary
+# n. me["teaching"] - list
+# o. me["teaching"][0]["semester"] - list
+# p is a trick question look at it carefully - integer
+# p. me["teaching"][0]["coursename"] - no such variable type
